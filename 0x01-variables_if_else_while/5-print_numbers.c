1df4f3042230305b3353b35f323f444828c7de8c
Pastoreekahk <apastoreekahk96@gmail.com>
2023-09-11 18:21:04 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = '0' ; a <= '9' ; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
