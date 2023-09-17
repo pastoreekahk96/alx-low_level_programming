1680dd4f20c4a9b30f16b54e36a610b06a0c81a1
Pastoreekahk <apastoreekahk96@gmail.com>
2023-09-16 22:04:28 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main - A C program that prints a line not include print to stdout
  *
- * Return: 1 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar 2015-10-19\n", stdout);
 	return (1);
 }
